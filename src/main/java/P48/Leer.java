b3fef8028774de648dea6eaa372815f9bbacf855
GonAfMont <gonzaloafonsomon@gmail.com>
2024-05-14 01:59:25 +0100

    Práctica 48 Maven

---
+++ src/main/java/P48/Leer.java
@@ -0,0 +1,15 @@
+package P48;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name="autos")
+@XmlAccessorType(XmlAccessType.FIELD)
+
+
+public class Leer {
+
+	public void leerXml() {
+		
+	}
+
+}
